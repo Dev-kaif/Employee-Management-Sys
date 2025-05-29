dc2636e8f5493f099dc19cbca8fb4244a6e42168
Mohammad Kaif <kaifghalib123@gmail.com>
2025-05-30 00:18:53 +0530

    main index.js

---
+++ employee-management-backend/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import authRoutes from "./routes/authRoutes";
+import employeeRoutes from "./routes/employeeRoutes";
+import shiftRoutes from "./routes/shiftRoutes";
+import { Mongo_Db_URL } from "./config/config";
+
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/employees", employeeRoutes);
+app.use("/api/shifts", shiftRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+async function main() {
+  try {
+    await mongoose.connect(Mongo_Db_URL);
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  } catch (err) {
+    console.error(" MongoDB connection failed:", err);
+    process.exit(1);
+  }
+}
+
+main()
