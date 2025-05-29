41b8e368ea7c3e784249e35b354105cfff009783
Mohammad Kaif <kaifghalib123@gmail.com>
2025-05-30 00:16:25 +0530

    made login function for the employess and admin

---
+++ employee-management-backend/src/controllers/authController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { Employee } from "../models/employeeModel";
+import { JWT_SECRET } from "../config/config";
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await Employee.findOne({ email });
+
+    if (!user) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(401).json({ message: "Invalid credentials" });
+      return;
+    }
+
+    const token = jwt.sign({ userId: user._id, role: user.role }, JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
